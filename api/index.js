1f2b4007b878101f7b2655fc33474d2c1a5352ab
Mark <65377051+markmo123@users.noreply.github.com>
2026-05-06 15:33:14 +0800

    add index.js for funcs

---
+++ api/index.js
@@ -0,0 +1,4 @@
+// This file loads all the functions for the Azure Functions runtime
+require('./readings-get/index.js');
+require('./readings-history/index.js');
+require('./readings-post/index.js');
